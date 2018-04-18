0a38ab8ddd2e4e892e7709f3c4dd89b66aeccb84
Ivan Anyaegbu <ioa13a@lib-301-07.acu.local>
2018-04-18 16:09:47 -0500

    Fixed issuse of initial code with help from Darius

--- JsonBetterThanXml/DetailViewController.swift
+++ JsonBetterThanXml/DetailViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
@@ -21,25 +21,25 @@ class DetailViewController: UIViewController {
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
+    
+    var detailItem: String? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    
 }
 
